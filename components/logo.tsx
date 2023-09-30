31b8f15b279dabeee814ec6e859304ada92ea378
Tomek Marciniak <tmarciniakm@gmail.com>
2023-09-30 23:51:18 +0200

    fix(logo): fix for system dark

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,8 +2,8 @@ import { useTheme } from 'next-themes'
 import NextImage from 'next/image'
 
 export const Logo = () => {
-  const { theme } = useTheme()
-  const darkTheme = theme === 'dark'
+  const { theme, systemTheme } = useTheme()
+  const darkTheme = theme === 'dark' || (theme === 'system' && systemTheme === 'dark')
   return (
     <div>
       {darkTheme ? <NextImage src="/logo-dark.svg" width={120} height={40} alt="Logo Dark" id="logoDark" /> :
