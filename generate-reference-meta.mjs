40654a9b80f8f438c0ce22909522a2511bf6bcfa
Matej Sima <matej.sima@gmail.com>
2025-02-04 14:05:36 +0100

    added a log message to typedoc meta generator script

--- generate-reference-meta.mjs
+++ generate-reference-meta.mjs
@@ -23,6 +23,8 @@ function referenceToSidebarTitle(reference) {
   return `@proto-kit/${reference}`;
 }
 
+console.log("generating typedoc meta files...");
+
 references.forEach((reference) => {
   const metaTsxPath = `./src/pages/docs/reference/${reference}/_meta.tsx`;
   fs.rmSync(metaTsxPath, { force: true });
