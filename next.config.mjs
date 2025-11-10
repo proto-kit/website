aa40a5b3bac12adbf46c02d8c3221a660d32b9a5
stanlou <ghassen.loukil.it@gmail.com>
2025-11-10 23:53:00 +0100

    deploy on gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,17 @@ const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   latex: true,
-  search: {
-    codeblocks: false,
-  },
+  search: { codeblocks: false },
 });
+
+const isProd = process.env.NODE_ENV === "production";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 export default withNextra({
   reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: isProd ? basePath : "",
+  assetPrefix: isProd ? `${basePath}/` : "",
 });
